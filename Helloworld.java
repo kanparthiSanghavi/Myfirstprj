021f6cf2169cf7c22254c92e31df35e83c09edfa
kanparthiSanghavi <135301751+kanparthiSanghavi@users.noreply.github.com>
2023-06-02 10:39:44 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,39 @@
+package secondproj;
+
+//public class Helloworld {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method 
+//
+//		System.out.println("Hello World!!!");
+//
+//
+//	}
+//
+//}
+
+public class Helloworld {
+	
+	public void myMethod1() {
+		System.out.println(" myMethod1 ");
+	}
+	
+	public void myMethod2() {
+		System.out.println(" myMethod2 ");
+	}
+	
+	public void mymethod3() {
+		System.out.println(" mymethod3 ");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Helloworld h = new Helloworld();
+		
+		h.myMethod2();
+
+	}
+
+}
+
